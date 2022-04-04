0d0f2e2115f9518eb22c08c0c89e13a8aa307c6b
Christopher Lemire <goodbye300@aim.com>
2022-04-04 01:05:14 -0500

    Completed Video Tutorial 72

--- core/src/ch/expectusafterlun/game/MyGdxGame.java
+++ core/src/ch/expectusafterlun/game/MyGdxGame.java
@@ -1,21 +1,42 @@
 package ch.expectusafterlun.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 
+	private SpriteBatch batch;
+	private BitmapFont font;
+
 	@Override
 	public void create () {
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		font.setColor(Color.BLUE);
+		font.getData().setScale(5);
 	}
 
 	@Override
 	public void render () {
+		// Whipe the screen, so we are working with a blank canvas
+		Gdx.gl.glClearColor(1, 1, 1, 1); // White
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.begin();
+		// 0,0 is the bottom left
+		font.draw(batch, "Hello baby! Don't say maybe!", 100, 400);
+		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
+		batch.dispose();
+		font.dispose();
 	}
 }
